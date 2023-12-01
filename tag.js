e1708bdcec3717df79a7ca5443d7ba64ecb4b060
AlinaGerman <151366227+AlinaGerman@users.noreply.github.com>
2023-12-01 22:44:56 +0700

    Update tag.js

--- tag.js
+++ tag.js
@@ -1,7 +1,7 @@
 var canvas=document.getElementById('canvas')
 var ctx=canvas.getContext('2d')
 var elements=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-
+var el=[]
 function mix(elements){
     for (let i=0;i<elements.length;i++){
         let id=Math.floor(Math.random()*(i+1))
@@ -9,8 +9,13 @@ function mix(elements){
         elements[i]=elements[id]
         elements[id]=current
     }
-    return elements
+    el[0]=elements.slice(0,4)
+    el[1]=elements.slice(4,8)
+    el[2]=elements.slice(8,12)
+    el[3]=elements.slice(12,16)
+    return el
 }
+console.log(mix(elements))
 
 function draw_square(x, y, number){
     ctx.fillStyle='black'
@@ -23,9 +28,17 @@ function draw_square(x, y, number){
         ctx.fillRect(x+5, y+5, 90, 90)
         ctx.font='60px Arial'
         ctx.fillStyle='black'
-        ctx.fillText(number,x+35, y+70)
-    }
-    
+        if (number<10){
+            ctx.fillText(number,x+35, y+70)
+        }else{
+            ctx.fillText(number, x+15, y+70)
+        }
+    }    
 }
 
-draw_square(200, 200, 7)
+//draw_square(200, 200, 4)
+function draw_tag(elements){
+    for (let i=0;i<elements.length;i++){
+        
+    }
+}

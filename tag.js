d9855aa62ceb82d49cafeae6c6b9c34d5abad324
AlinaGerman <151366227+AlinaGerman@users.noreply.github.com>
2023-12-01 20:51:11 +0700

    Update tag.js

--- tag.js
+++ tag.js
@@ -2,16 +2,13 @@ var canvas=document.getElementById('canvas')
 var ctx=canvas.getContext('2d')
 var elements=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 
-function mix(elemnents){
-    var new_elements=[]
-    var ids=[]
-    while (ids.length<elements.length){
-        let id=Math.round(Math.random()*10)
-        if (id>0 && id<elements.length && !(ids.includes(id))){
-            ids.push(id)
-        }
+function mix(elements){
+    for (let i=0;i<elements.length;i++){
+        let id=Math.floor(Math.random()*(i+1))
+        let current=elements[i]
+        elements[i]=elements[id]
+        elements[id]=current
     }
-    console.log(ids)
-    
+    return elements
 }
-mix(elements)
+console.log(mix(elements))

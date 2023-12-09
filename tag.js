6e5f9a2099edb5178ccba83d525d29fca1534d3d
AlinaGerman <151366227+AlinaGerman@users.noreply.github.com>
2023-12-09 22:50:39 +0700

    Update tag.js

--- tag.js
+++ tag.js
@@ -5,6 +5,7 @@ var help_matrix=[]
 var matrix=[]
 
 function mix(elements){
+    let matrix=[]
     for (let i=0;i<elements.length;i++){
         let id=Math.floor(Math.random()*(i+1))
         let current=elements[i]
@@ -41,7 +42,7 @@ function draw_square(x, y, number){
     }    
 }
 
-function draw_tag(matrix){
+function draw_tag(){
     for (let i=0;i<matrix.length;i++){
         for (let j=0;j<matrix[i].length;j++){
             draw_square(100*i, 100*j, matrix[i][j])
@@ -52,16 +53,36 @@ function draw_tag(matrix){
 function find_positsion_empty(mat){
     var positsion_empty=[]
     for (let i=0;i<mat.length;i++){
-        for (let j=0;i<mat[i].length;j++){
+        for (let j=0;j<mat[i].length;j++){
             if (mat[i][j]==0){
-                return [i, j]
+                positsion_empty[0]=i
+                positsion_empty[1]=j
             }
         }
     }
+    return positsion_empty
+}
+function move(x,y){
+    let empty=find_positsion_empty(matrix)
+    if (((empty[1]==y-1 || empty[1]==y+1) && (empty[0]==x)) || ((empty[0]==x-1 || empty[0]==x+1) && empty[1]==y)){
+        matrix[empty[0]][empty[1]]=matrix[x][y]
+        matrix[x][y]=0
+    }
 }
 
-var m = mix(elements)
-console.log(m)
-draw_tag(m)
-console.log(find_positsion_empty(m))
+function find_click(event){
+    let column=Math.floor(event.clientX/100)
+    let line=Math.floor(event.clientY/100)
+    move(column,line)
+    draw_tag()
 
+}
+
+
+canvas.addEventListener('click', find_click)
+
+//var matrix = mix(elements)
+var matrix=[[1,5,9,13],[2,6,10,14],[3,7,11,0],[4,8,12,15]]
+console.log(matrix)
+find_positsion_empty(matrix)
+draw_tag()
